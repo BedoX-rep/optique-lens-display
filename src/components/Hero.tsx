cef099647a4263c44504d35ceb71f704c3b1ea55
BedoX-rep <kaka.005shit@gmail.com>
2025-07-01 19:15:53 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-
 import { Button } from "@/components/ui/button";
 import { RotateCcw, Shield, MapPin, Lock } from "lucide-react";
 
@@ -9,7 +8,8 @@ const Hero = () => {
         {/* Decorative gradient overlay */}
         <div className="absolute inset-0 z-0 pointer-events-none">
           <div className="w-full h-full bg-gradient-to-tr from-blue-200/60 via-transparent to-blue-400/40"></div>
-        </div> <div className="absolute inset-0">
+        </div>
+        <div className="absolute inset-0">
           <img 
             src="/attached_images/banner2.png" 
             alt="Hero banner" 
@@ -58,30 +58,11 @@ const Hero = () => {
             </div>
           </div>
 
-          {/* Glasses positioned around the content */}
+          {/* Decorative elements */}
           <div className="absolute inset-0 pointer-events-none">
-            {/* Top left glasses */}
-            <div className="absolute top-4 left-4 lg:top-8 lg:left-8">
-              <div className="w-16 h-10 lg:w-20 lg:h-12 bg-black bg-opacity-70 rounded-lg transform -rotate-12 shadow-xl"></div>
-              <span className="absolute -bottom-4 left-1 text-white text-xs font-medium">Arthur</span>
-            </div>
-
-            {/* Top right glasses */}
+            {/* Top right circle (replacing Jane rectangle and name) */}
             <div className="absolute top-4 right-4 lg:top-8 lg:right-8">
-              <div className="w-16 h-10 lg:w-20 lg:h-12 bg-black bg-opacity-70 rounded-lg transform rotate-12 shadow-xl"></div>
-              <span className="absolute -bottom-4 right-1 text-white text-xs font-medium">Jane</span>
-            </div>
-
-            {/* Bottom left glasses */}
-            <div className="absolute bottom-4 left-4 lg:bottom-8 lg:left-8">
-              <div className="w-16 h-10 lg:w-20 lg:h-12 bg-amber-600 bg-opacity-80 rounded-lg transform rotate-6 shadow-xl"></div>
-              <span className="absolute -bottom-4 left-1 text-white text-xs font-medium">Bathany</span>
-            </div>
-
-            {/* Bottom right glasses */}
-            <div className="absolute bottom-4 right-4 lg:bottom-8 lg:right-8">
-              <div className="w-16 h-10 lg:w-20 lg:h-12 bg-amber-700 bg-opacity-80 rounded-lg transform -rotate-6 shadow-xl"></div>
-              <span className="absolute -bottom-4 right-1 text-white text-xs font-medium">Frances</span>
+              <div className="w-16 h-16 lg:w-20 lg:h-20 bg-blue-200 bg-opacity-80 rounded-full shadow-xl"></div>
             </div>
 
             {/* Additional decorative circles */}
