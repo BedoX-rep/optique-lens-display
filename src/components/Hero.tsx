b751fd64d0e8f1d394220338e5314df804693cb4
BedoX-rep <kaka.005shit@gmail.com>
2025-07-01 19:13:44 +0100

    Revert "Update Hero.tsx"
    This reverts commit 64317f481e865bba4d012558b4b30840063077aa.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+
 import { Button } from "@/components/ui/button";
 import { RotateCcw, Shield, MapPin, Lock } from "lucide-react";
 
@@ -8,14 +9,13 @@ const Hero = () => {
         {/* Decorative gradient overlay */}
         <div className="absolute inset-0 z-0 pointer-events-none">
           <div className="w-full h-full bg-gradient-to-tr from-blue-200/60 via-transparent to-blue-400/40"></div>
-        </div>
-        <div className="absolute inset-0">
+        </div> <div className="absolute inset-0">
           <img 
             src="/attached_images/banner2.png" 
             alt="Hero banner" 
             className="w-full h-full object-cover"
           />
-          {/* Removed blue overlay */}
+          <div className="absolute inset-0 bg-blue-400 bg-opacity-30"></div>
         </div>
 
         <div className="px-4 py-8 relative z-10 h-full">
@@ -63,25 +63,25 @@ const Hero = () => {
             {/* Top left glasses */}
             <div className="absolute top-4 left-4 lg:top-8 lg:left-8">
               <div className="w-16 h-10 lg:w-20 lg:h-12 bg-black bg-opacity-70 rounded-lg transform -rotate-12 shadow-xl"></div>
-              {/* Removed name */}
+              <span className="absolute -bottom-4 left-1 text-white text-xs font-medium">Arthur</span>
             </div>
 
             {/* Top right glasses */}
             <div className="absolute top-4 right-4 lg:top-8 lg:right-8">
               <div className="w-16 h-10 lg:w-20 lg:h-12 bg-black bg-opacity-70 rounded-lg transform rotate-12 shadow-xl"></div>
-              {/* Removed name */}
+              <span className="absolute -bottom-4 right-1 text-white text-xs font-medium">Jane</span>
             </div>
 
             {/* Bottom left glasses */}
             <div className="absolute bottom-4 left-4 lg:bottom-8 lg:left-8">
               <div className="w-16 h-10 lg:w-20 lg:h-12 bg-amber-600 bg-opacity-80 rounded-lg transform rotate-6 shadow-xl"></div>
-              {/* Removed name */}
+              <span className="absolute -bottom-4 left-1 text-white text-xs font-medium">Bathany</span>
             </div>
 
             {/* Bottom right glasses */}
             <div className="absolute bottom-4 right-4 lg:bottom-8 lg:right-8">
               <div className="w-16 h-10 lg:w-20 lg:h-12 bg-amber-700 bg-opacity-80 rounded-lg transform -rotate-6 shadow-xl"></div>
-              {/* Removed name */}
+              <span className="absolute -bottom-4 right-1 text-white text-xs font-medium">Frances</span>
             </div>
 
             {/* Additional decorative circles */}
